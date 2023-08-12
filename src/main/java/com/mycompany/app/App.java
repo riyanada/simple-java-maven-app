3783c2e17e55e92fc6e5185e7f84b26e096a6cb2
Riyan <riyanmaulana402@yahoo.co.id>
2023-08-12 23:53:21 +0700

    add datadiri

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,11 +6,15 @@ package com.mycompany.app;
 public class App {
 
     private static final String MESSAGE = "Hello World!";
+    private static final String name = "Riyan";
+    private static final String username = "riyanada";
 
     public App() {}
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
+        System.out.println("Nama: " + name;)
+        System.out.println("Username: " + username;)
     }
 
     public String getMessage() {
